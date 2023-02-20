2cb064e700fcbd597e57e878934b548ac3ea40e5
Adachris <okafordoris70@yahoo.com>
2023-02-20 13:48:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-clear
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
-
-
-
-
-
-
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
