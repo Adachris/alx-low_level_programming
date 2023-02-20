eb152cb23b6c477af38f38d2b0b1cab54430ee14
Adachris <okafordoris70@yahoo.com>
2023-02-20 13:39:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#!/bin/bash
-#include<stdio.h>
-/**
- * main -is the entry point for the program.
- *
- * Return:zero if no error, Non-zero value if errors
- * */
-int main(void)
-{
- puts("\Programmng is like bulding a multilingual puzzle");
- return (0);
-}
+clear
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+e x00. C- Hello, World
