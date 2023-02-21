783495682a503a248b73b56827470b9c884d95bb
Adachris <okafordoris70@yahoo.com>
2023-02-21 14:01:56 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
+#include <stdio.h>
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
+/**
+ * main - To write a program that prints _putchar
+ *
+ * Return: 0 (success)
+ */
 
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
-
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		putchar('\n');
+			return (0);
+}
+Footer
+© 2023 GitHub, Inc.
